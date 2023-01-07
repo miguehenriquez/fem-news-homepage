3382c10f0fb0f977764cba59c5075d06817c79fb
Miguel Henriquez <miguelisaiashenriquez>
2023-01-06 20:14:46 -0600

    add tailwindcss and initial menu styles on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-orange": "hsl(35, 77%, 62%)",
+        "soft-red": "hsl(5, 85%, 63%)",
+        "off-white": "hsl(36, 100%, 99%)",
+        "grayish-blue": "hsl(233, 8%, 79%)",
+        "dark-grayish-blue": "hsl(236, 13%, 42%)",
+        "very-dark-blue": "hsl(240, 100%, 5%)",
+      },
+      fontSize: "15px",
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
