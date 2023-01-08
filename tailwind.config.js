6ffe33780fd68fe6a6ba87e0aa5cc42caa9dc181
Miguel Henriquez <miguelisaiashenriquez>
2023-01-08 08:54:13 -0600

    add initial main content style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "soft-orange": "hsl(35, 77%, 62%)",
-        "soft-red": "hsl(5, 85%, 63%)",
-        "off-white": "hsl(36, 100%, 99%)",
-        "grayish-blue": "hsl(233, 8%, 79%)",
-        "dark-grayish-blue": "hsl(236, 13%, 42%)",
-        "very-dark-blue": "hsl(240, 100%, 5%)",
+        "c-soft-orange": "hsl(35, 77%, 62%)",
+        "c-soft-red": "hsl(5, 85%, 63%)",
+        "c-off-white": "hsl(36, 100%, 99%)",
+        "c-grayish-blue": "hsl(233, 8%, 79%)",
+        "c-dark-grayish-blue": "hsl(236, 13%, 42%)",
+        "c-very-dark-blue": "hsl(240, 100%, 5%)",
       },
       fontSize: "15px",
       fontFamily: {
